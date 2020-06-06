f7765e749c2ee4ed4809e7544be6b4bc76896834
PhamPhi <phammanhphi3011@gmail.com>
2020-06-06 17:04:50 +0700

    fix  layout moved on page

--- newslettter.php
+++ newslettter.php
@@ -20,13 +20,13 @@
                             </div>
 							<ul class="newsletter-follow">
 								<li>
-									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="https://www.facebook.com/boylangtumaimaiyeuem"><i class="fa fa-facebook"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="https://www.facebook.com/boylangtumaimaiyeuem"><i class="fa fa-twitter"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="fa fa-instagram"></i></a>
+									<a href="https://www.facebook.com/boylangtumaimaiyeuem"><i class="fa fa-instagram"></i></a>
 								</li>
 								
 							</ul>
