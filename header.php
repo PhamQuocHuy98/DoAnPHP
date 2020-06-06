d792238caf6f9f4a952c26f9c9ac13ddfbfb4c41
Ben Vo <garoben998@gmail.com>
2020-06-06 16:56:22 +0700

    fix color style css

--- header.php
+++ header.php
@@ -46,11 +46,11 @@ session_start();
 		<![endif]-->
 	<style>
 		#navigation {
-			background: #FF4E50;
+			background: #FFFFFF;
 			/* fallback for old browsers */
 			background: -webkit-linear-gradient(to right, #F9D423, #FF4E50);
 			/* Chrome 10-25, Safari 5.1-6 */
-			background: linear-gradient(to right, #F9D423, #FF4E50);
+			background: linear-gradient(to right, ##FFFFFF, ##FFFFFF);
 			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 
@@ -62,7 +62,7 @@ session_start();
 			/* fallback for old browsers */
 			background: -webkit-linear-gradient(to right, #061161, #780206);
 			/* Chrome 10-25, Safari 5.1-6 */
-			background: linear-gradient(to right, #061161, #780206);
+			background: linear-gradient(to right, #00b4db, #00b4db);
 			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 
@@ -75,7 +75,7 @@ session_start();
 			/* fallback for old browsers */
 			background: -webkit-linear-gradient(to right, #190A05, #870000);
 			/* Chrome 10-25, Safari 5.1-6 */
-			background: linear-gradient(to right, #190A05, #870000);
+			background: linear-gradient(to right, #60afdd, #60afdd);
 			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 
@@ -86,11 +86,11 @@ session_start();
 			/* fallback for old browsers */
 			background: -webkit-linear-gradient(to right, #348AC7, #7474BF);
 			/* Chrome 10-25, Safari 5.1-6 */
-			background: linear-gradient(to right, #348AC7, #7474BF);
+			background: linear-gradient(to right, #00b4db, #00b4db);
 			/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 
-			color: #1E1F29;
+			color: #FFF	;
 		}
 
 		#bottom-footer {
@@ -105,7 +105,7 @@ session_start();
 		}
 
 		.footer-links li a {
-			color: #1E1F29;
+			color: #FFF;
 		}
 
 		.mainn-raised {
